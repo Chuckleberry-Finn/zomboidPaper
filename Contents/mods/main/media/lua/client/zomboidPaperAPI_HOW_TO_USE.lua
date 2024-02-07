dab31b29a2056da053fa6dc054b591f9c7fef234
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-02-06 22:12:56 -0500

    init

---
+++ Contents/mods/main/media/lua/client/zomboidPaperAPI_HOW_TO_USE.lua
@@ -0,0 +1,21 @@
+return --- RETURN TO STOP THIS FROM BEING READ NORMALLY
+
+local paper = require "zomboidPaperAPI_define"
+paper.applyPaperInit("EXAMPLE_ITEM")
+
+--- ITEM SCRIPT
+
+--[[
+
+    item EXAMPLE_ITEM {
+		DisplayCategory = Literature,
+		Weight = 0.1,
+		Type = Map,
+		DisplayName = EXAMPLE ITEM,
+		Icon = EXAMPLE_ITEM,
+		Map = paperAPI_CharacterSheet,
+		WorldStaticModel = EXAMPLE_ITEM,
+		paperAPI_paperPageMax = 2,
+    }
+
+--]]
